f913316c1812d6938716a25d4a6d7d83b72554b5
BuildTools <unconfigured@null.spigotmc.org>
2021-12-15 22:52:16 +0700

    update homepage

--- homepage.php
+++ homepage.php
@@ -1,22 +1,31 @@
 <!DOCTYPE html>
 <html lang="en" dir="ltr">
-  <head>
-    <meta charset="utf-8">
-    <title>Sign In | Vacci.me</title>
-    <link rel="stylesheet" href="style2.css">
-    <link rel="stylesheet" href="css/bootstrap.min.css" rel="stylesheet">
-  </head>
-  <body>
-    <div class="center">
-    <form id="form1" name="form1" method="post" action="halaman_signin.php">
-        <input type="submit" value="Sign In">
-    </form>
-    <form id="form1" name="form1" method="post" action="halaman_signup.php">
-        <input type="submit" value="Sign Up">
-    </form>
+
+<head>
+  <meta charset="utf-8">
+  <title>Vacci.me</title>
+  <link rel="stylesheet" href="style2.css">
+  <link rel="stylesheet" href="css/bootstrap.min.css" rel="stylesheet">
+</head>
+
+<body>
+  <div class="card col-md-3 ms-md-auto" style="height: 50rem;width: 50rem;">
+    <div class="text-center" style="padding-top:175px">
+      <h1 class="display-3" style="font-weight: bold;"><ins>Vacci.me</ins></h1>
+      <h5 style="font-weight: bold;padding-top:25px">Check your Vaccination data today</h5>
+      <h5 style="font-weight: bold;">Join us now!</h5>
+      <form id="form1" name="form1" method="post" action="halaman_signin.php" style="padding-top: 25px;">
+        <button class="btn btn-lg btn-primary btn-block" type="submit" style="width: 18rem;">Sign In</button>
+      </form>
+      <p style="padding-top: 10px;">Already have an account?</p>
+      <form id="form1" name="form1" method="post" action="halaman_signup.php">
+        <button class="btn btn-lg btn-primary btn-block" type="submit" style="width: 18rem;">Sign Up</button>
+      </form>
       <div class="line2">
       </div>
     </div>
+  </div>
 
-  </body>
+</body>
+
 </html>
